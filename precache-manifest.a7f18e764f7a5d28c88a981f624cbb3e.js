a08216610e38ff9919b845fa11cbbbb8d05f0d8e
Asterix <charlesrichson@gmail.com>
2019-09-18 18:20:27 +0100

    Updates

---
+++ precache-manifest.a7f18e764f7a5d28c88a981f624cbb3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9807ce4deafada76c6804d312ca1a833",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "27effbdfcdfd46d7369d",
+    "url": "/contactmanager/static/css/main.2d6b1f80.chunk.css"
+  },
+  {
+    "revision": "2c14063ee35a74940750",
+    "url": "/contactmanager/static/js/2.450ae6e0.chunk.js"
+  },
+  {
+    "revision": "27effbdfcdfd46d7369d",
+    "url": "/contactmanager/static/js/main.17b19303.chunk.js"
+  },
+  {
+    "revision": "a641aece7b2b99d3c792",
+    "url": "/contactmanager/static/js/runtime~main.fa8abcf3.js"
+  }
+]);
