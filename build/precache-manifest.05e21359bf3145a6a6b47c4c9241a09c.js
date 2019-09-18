75c0a563918ffec09b7dbae22ce3f080758f886c
Asterix <charlesrichson@gmail.com>
2019-09-18 18:07:38 +0100

    first commit

---
+++ build/precache-manifest.05e21359bf3145a6a6b47c4c9241a09c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20d2f937600fde132eebe32b95e79ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b02c4267c57128828529",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d53ed0b64edb374bc196",
+    "url": "/static/js/2.a3a7ffee.chunk.js"
+  },
+  {
+    "revision": "b02c4267c57128828529",
+    "url": "/static/js/main.9c7609ef.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import "./css/bootstrap.css";
+import "./App.css";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
+import Header from "./components/layout/Header";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+import Test from "./components/test/Test";
+import { Provider } from "./context";
+
+function App() {
+  return (
+    <Provider>
+      <Router>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Contacts}></Route>
+              <Route exact path="/contact/add" component={AddContact}></Route>
+              <Route
+                exact
+                path="/contact/edit/:id"
+                component={EditContact}
+              ></Route>
+              <Route exact path="/about" component={About}></Route>
+              <Route exact path="/test" component={Test}></Route>
+              <Route component={NotFound}></Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
